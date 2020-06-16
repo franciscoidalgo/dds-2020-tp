c08f36b6e583a00c0b717f3f3f5adcd99770d6a9
xyz-frba <afraile@est.frba.utn.edu.ar>
2020-06-16 07:59:57 -0300

    Update Validador de passowrds: Main --> Test

--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -2,20 +2,6 @@ package Principal;
 
 // import java.util.Scanner;
 
-import Password.PasswordValidator;
-import Password.ValidatePasswordLength;
-
-public class Main {
-
-    public static void main (String[] args) {
-
-        System.out.println("Hello, world!");
-
-    }
-
-}
-
-
 /*
 
  public static void main (String[] args)
