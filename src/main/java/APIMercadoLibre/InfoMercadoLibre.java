4a3a510a9227d3c7d895b148bcd54f49525c8975
francisoidalgo <fidalgo@est.frba.utn.edu.ar>
2020-08-22 20:30:34 -0300

    Tests comentados, prueba con persistencia en memoria, mas funcionalidad a InfoMercadoLibre

--- src/main/java/APIMercadoLibre/InfoMercadoLibre.java
+++ src/main/java/APIMercadoLibre/InfoMercadoLibre.java
@@ -1,17 +1,23 @@
 package APIMercadoLibre;
 
+import APIMercadoLibre.modelos.Identificable;
 import APIMercadoLibre.modelos.Moneda;
 import APIMercadoLibre.modelos.Pais;
+import APIMercadoLibre.modelos.Provincia;
 import APIMercadoLibre.services.ServicioMercadoLibre;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class InfoMercadoLibre {
+
     private List<Pais> listaDePaises;
+    private List<Provincia> listaDeProvincias;
     private List<Moneda> listaDeMonedas;
     private static InfoMercadoLibre instancia = null;
-    private ServicioMercadoLibre servicioMercadoLibre;
+    private static ServicioMercadoLibre servicioMercadoLibre;
 
     private InfoMercadoLibre() throws IOException {
         this.actualizarInfo();
@@ -19,6 +25,7 @@ public class InfoMercadoLibre {
 
     public static InfoMercadoLibre instancia () throws IOException {
         if (instancia == null){
+            servicioMercadoLibre = ServicioMercadoLibre.instancia();
             instancia = new InfoMercadoLibre();
         }
         return instancia;
@@ -27,18 +34,50 @@ public class InfoMercadoLibre {
     public void actualizarInfo() throws IOException {
         listaDePaises = servicioMercadoLibre.listaDePaisesConProvincias();
         listaDeMonedas = servicioMercadoLibre.listaDeMonedas();
+        listaDeProvincias = new ArrayList<>();
+        for (Pais pais : listaDePaises){
+            for (Identificable provincia : pais.mostrarProvincias()){
+                listaDeProvincias.add(servicioMercadoLibre.provinciaDeId(provincia.id));
+            }
+        }
     }
 
     public void persistirInfo(){
         //TODO
     }
 
-
     public List<Pais> getListaDePaises() {
         return listaDePaises;
+    }
+
+    public Pais paisDeNombre (String nombre) throws Exception {
+
+        Optional<Pais> paisBuscado = listaDePaises.stream().filter(pais->pais.name.equals(nombre)).findFirst();
+        if(paisBuscado.isPresent()){
+            return paisBuscado.get();
+        }else{
+            throw new Exception("No se encontro el pais correspondiente a ese nombre.");
+        }
+
+    }
+
+    public Provincia provinciaDeNombre (String nombre) throws Exception {
+
+        Optional<Provincia> provinciaBuscada = listaDeProvincias.stream().filter(provincia->provincia.name.equals(nombre)).findFirst();
+        if(provinciaBuscada.isPresent()){
+            return provinciaBuscada.get();
+        }else{
+            throw new Exception("No se encontro el pais correspondiente a ese nombre.");
+        }
+
+    }
+
+    public List<Provincia> getListaDeProvincias(){
+        return listaDeProvincias;
     }
 
     public List<Moneda> getListaDeMonedas() {
         return listaDeMonedas;
     }
+
 }
--- src/main/java/APIMercadoLibre/TestApi.java
+++ src/main/java/APIMercadoLibre/TestApi.java
@@ -8,8 +8,9 @@ import java.util.Scanner;
 
 public class TestApi {
     public static void main(String[] args) throws Exception {
-        ServicioMercadoLibre servicioMercadoLibre = ServicioMercadoLibre.instancia();
-
+        InfoMercadoLibre infoMercadoLibre = InfoMercadoLibre.instancia();
+        List<Pais> paises = infoMercadoLibre.getListaDePaises();
+        List<Provincia> provincias = infoMercadoLibre.getListaDeProvincias();
         /*
         //Prueba: imprimir descripciones de las monedas
         List<Moneda> listaDeMonedas = servicioMercadoLibre.listaDeMonedas();
@@ -21,26 +22,26 @@ public class TestApi {
 
         //Prueba: Informacion del pais, la provincia y la ciudad basandose en el codigo postal.
         System.out.println("Lista de paises");
-        servicioMercadoLibre.listaDePaises().mostrarNombres();
+        for(Pais pais : paises){
+            System.out.println(pais.name);
+        }
         System.out.println("Ingrese el nombre de su pais: ");
         Scanner entradaScanner = new Scanner(System.in);
         String nombrePaisElegido = entradaScanner.nextLine();
-        Pais paisElegido = servicioMercadoLibre.paisDeNombre(nombrePaisElegido);
-        servicioMercadoLibre.listaDeProvincias(paisElegido).mostrarNombres();
+        Pais paisElegido = infoMercadoLibre.paisDeNombre(nombrePaisElegido);
+        for(Identificable provincia : paisElegido.mostrarProvincias()){
+            System.out.println(provincia.name);
+        }
         System.out.println("Ingrese el nombre de la provincia: ");
         String nombreProvinciaElegida = entradaScanner.nextLine();
-        Provincia provinciaElegida = servicioMercadoLibre
-                .provinciaDeId(servicioMercadoLibre.listaDeProvincias(paisElegido).idDeIdentificador(nombreProvinciaElegida));
-        servicioMercadoLibre.listaDeCiudades(provinciaElegida).mostrarNombres();
+        Provincia provinciaElegida = infoMercadoLibre.provinciaDeNombre(nombreProvinciaElegida);
+        for(Ciudad ciudad : provinciaElegida.cities){
+            System.out.println(ciudad.name);
+        }
         System.out.println("Ingrese el nombre de la ciudad: ");
         String nombreCiudadElegida = entradaScanner.nextLine();
-        Ciudad ciudadElegida = servicioMercadoLibre
-                .ciudadDeId(servicioMercadoLibre.listaDeCiudades(provinciaElegida).idDeIdentificador(nombreCiudadElegida));
         System.out.println("Ingrese su codigo postal: ");
         String codigoPostal = entradaScanner.nextLine();
-
-        CodigoPostalAPI informacionCodigoPostal = servicioMercadoLibre
-                .informacionCodigoPostal(servicioMercadoLibre.paisDeNombre(nombrePaisElegido).id, codigoPostal);
 
         entradaScanner.close();
 
