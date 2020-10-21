7d8f81b562a5dce26d614f815ecd8412f9ef6664
franciscoidalgo <giugno2407>
2020-10-21 19:51:28 -0300

    server proporcionado por heroku

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class Server {
     public static void main(String[] args) throws IOException {
         Spark.port(Integer.parseInt(System.getenv("PORT")));
+        //Spark.port(9000);
         Router.init();
         DebugScreen.enableDebugScreen();
     }
