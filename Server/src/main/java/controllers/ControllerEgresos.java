71100f1e2cfce6ee595d3291ecab4aee2d7ee1be
francisoidalgo <fidalgo@est.frba.utn.edu.ar>
2020-11-13 17:11:23 -0300

    Persistir path del comprobante.

--- Server/src/main/java/controllers/ControllerEgresos.java
+++ Server/src/main/java/controllers/ControllerEgresos.java
@@ -291,14 +291,24 @@ public class ControllerEgresos extends Controller {
     }
 
     public Response submitImagen(Request request, Response response) throws IOException, ServletException {
+        int id = request.session().attribute("egreso_actual");
         if (request.raw().getAttribute("org.eclipse.jetty.multipartConfig") == null) {
             MultipartConfigElement multipartConfigElement = new MultipartConfigElement(System.getProperty("java.io.tmpdir"));
             request.raw().setAttribute("org.eclipse.jetty.multipartConfig", multipartConfigElement);
         }
         Part part = request.raw().getPart("file");
-        Path filePath = Paths.get("src/main/resources/comprobantes/"+"egreso-"+request.session().attribute("egreso_actual")+".png");
+        String pathString = generarPath(Integer.toString(id));
+        Repositorio<OperacionEgreso> repoEgresos = FactoryRepo.get(OperacionEgreso.class);
+        OperacionEgreso operacionEgreso = repoEgresos.buscar(id);
+        operacionEgreso.setPath(pathString);
+        repoEgresos.modificar(operacionEgreso);
+        Path filePath = Paths.get(pathString);
         Files.copy(part.getInputStream(), filePath);
         return response;
+    }
+
+    private String generarPath(String id){
+        return "src/main/resources/comprobantes/"+"egreso-"+ id +".png";
     }
 
 
