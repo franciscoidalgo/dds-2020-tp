81b18df50eabc5def09e96152d7882d5d81dc4f2
francisoidalgo <fidalgo@est.frba.utn.edu.ar>
2020-11-15 20:37:14 -0300

    Ya ni se

--- ComponenteVinculador/src/main/java/serverVinculador/ServerVinculador.java
+++ ComponenteVinculador/src/main/java/serverVinculador/ServerVinculador.java
@@ -7,8 +7,9 @@ import java.io.IOException;
 
 public class ServerVinculador {
     public static void main(String[] args) throws IOException {
-        Spark.port(9001);
+        Spark.port(Integer.parseInt(System.getenv("PORT")));
+        //Spark.port(9001);
         RouterVinculacion.init();
-        DebugScreen.enableDebugScreen();
+        //DebugScreen.enableDebugScreen();
     }
 }
