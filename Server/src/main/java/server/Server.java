0342e2f28bafb9d68bc267589463a23bf764554c
francisoidalgo <fidalgo@est.frba.utn.edu.ar>
2020-11-15 17:27:28 -0300

    Puerto de heroku

--- Server/src/main/java/server/Server.java
+++ Server/src/main/java/server/Server.java
@@ -11,7 +11,7 @@ import spark.Spark;
 public class Server {
     public static void main(String[] args) throws Exception {
         //iniciarCategoriasAFIP();
-        Spark.port(9000);
+        Spark.port(Integer.parseInt(System.getenv("PORT")));
         Router.init();
         //DebugScreen.enableDebugScreen();
     }
